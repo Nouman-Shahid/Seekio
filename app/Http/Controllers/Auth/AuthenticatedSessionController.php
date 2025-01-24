6e4bb940a166c08ef612ce540dfbab56a35a3665
Nouman-Shahid <noumanshahid2486@gmail.com>
2025-01-24 15:19:19 +0500

    Initial Commit

---
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * Display the login view.
+     */
+    public function create(): Response
+    {
+        return Inertia::render('Auth/Login', [
+            'canResetPassword' => Route::has('password.request'),
+            'status' => session('status'),
+        ]);
+    }
+
+    /**
+     * Handle an incoming authentication request.
+     */
+ public function store(LoginRequest $request): RedirectResponse
+    {
+        $request->authenticate();
+
+        $user = Auth::user();
+
+        $request->session()->regenerate();
+
+        // Redirect based on user role
+        if ($user->role == 'student') {
+            return redirect()->intended(route('dashboard'));
+        } elseif ($user->role == 'teacher') {
+            return redirect()->intended(route('teacherdashboard'));
+        } elseif ($user->role == 'admin') {
+            return redirect()->intended(route('admindashboard'));
+        }
+
+        return redirect()->route('home');
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
